dc1e0988ce9afb924acd86035db84758d9b84a18
GabrieliMarta <gabrielirocha2730@gmail.com>
2024-09-04 22:53:53 -0300

    Desenvolvendo home view #2

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {  RouterView } from 'vue-router'
+import HeaderVue from '@/components/others/HeaderVue.vue'
+</script>
+
+<template>
+  <HeaderVue />
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/others/HeaderVue.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <img class="logo-principal" src="/public/logoH.png">
+            <img class="logo" src="/public/logo-sushi.svg">
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    width: 100%;
+    padding: 20px;
+    margin: auto;
+    background-color: #1a1818e8;
+}
+
+.container{
+    display: flex;
+    flex-direction: column;
+    width: 50%;
+    margin: auto;
+    justify-content: center;
+    align-items: center;
+}
+</style>
