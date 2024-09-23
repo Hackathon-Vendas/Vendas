07779645524f5ff4c0bfe7c0f7b3c22134f78011
Henrique0330 <henriquebelboni@gmail.com>
2024-09-23 15:11:53 -0300

    feat: criação da 'EntradasView.vue' e começando sua personalização #14

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {  RouterView } from 'vue-router'
 import HeaderVue from '@/components/others/HeaderVue.vue'
+
 </script>
 
 <template>
---
+++ src/views/EntradasView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+import HeaderVue from '@/components/others/HeaderVue.vue';
+const pratos = [{
+    imagem: '',
+    titulo: 'temaki',
+    descricao: 'ishsofhs'
+}]
+</script>
+
+<template>
+    <HeaderVue />
+    <div class="container">
+        <h2>Entradas</h2>
+        <div class="cardapio">
+            <div class="pratos">
+                <div class="ingredientes"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
