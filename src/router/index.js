af803c59ca55737df649d37c9c75ad11b0ba1fb9
Henrique0330 <henriquebelboni@gmail.com>
2024-09-24 21:51:53 -0300

    feat:  Criação de divs e alinhamento e descrições delas  #14

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import EntradasView from '../views/EntradasView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/entradas',
+      name: 'entradas',
+      component: EntradasView   
+    }
     
   ]
 })
--- src/views/EntradasView.vue
+++ src/views/EntradasView.vue
@@ -1,25 +1,93 @@
 <script setup>
 import { ref } from 'vue';
-import HeaderVue from '@/components/others/HeaderVue.vue';
+import MenuVue from '@/components/others/MenuVue.vue';
+
 const pratos = [{
-    imagem: '',
+    imagem: '/public/imagem sumono.jpg',
+    titulo: 'Sunomono',
+    descricao: 'Salada refrescante de pepino agridoce com gergelim torrado.'
+},
+{
+    imagem: '/public/imagem sumono.jpg',
     titulo: 'temaki',
     descricao: 'ishsofhs'
 }]
 </script>
 
 <template>
-    <HeaderVue />
+    <MenuVue />
     <div class="container">
         <h2>Entradas</h2>
         <div class="cardapio">
-            <div class="pratos">
-                <div class="ingredientes"></div>
+            <div v-for="prato in pratos" :key="prato.titulo">
+                <div class="pratos">
+                    <img :src="prato.imagem" :alt="prato.titulo" class="Menu">
+                    <div class="descricao">
+                    <h3>{{ prato.titulo }}</h3>
+                    <p>{{ prato.descricao }}</p>
+                </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.cardapio {
+    justify-content: center;
+}
 
+.descricao{
+    padding-top: 2%;
+}
+
+.pratos {
+    display: flex;
+    border-radius: 8px;
+    margin-left: 25%;
+    width: 70%;
+    background-color: #454545;
+    margin-top: 2%;
+    color: #FFFFFF;
+}
+
+.Menu {
+    margin: 2%;
+    width: 18%;
+    border-radius: 8px;
+
+}
+
+main {
+    width: 90%;
+    margin: auto;
+    display: flex;
+}
+
+.sidebar {
+    height: 100vh;
+    width: 200px;
+    position: fixed;
+    top: 9, 5vh;
+    left: 0;
+    background-color: #333;
+    padding-top: 20px;
+}
+
+.sidebar ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+.sidebar ul li {
+    padding: 15px;
+    text-align: center;
+}
+
+.sidebar ul li a {
+    text-decoration: none;
+    color: white;
+    display: block;
+    font-size: 18px;
+}
 </style>
