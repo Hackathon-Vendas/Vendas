81084e5dfc6c76bcb848443c0e0b2eb1a6a415c6
g_vallentini <131276482+Vallentini@users.noreply.github.com>
2024-09-25 21:55:47 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import EntradasView from '../views/EntradasView.vue'
-<<<<<<< HEAD
-
-
-=======
->>>>>>> af803c59ca55737df649d37c9c75ad11b0ba1fb9
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,15 +13,8 @@ const router = createRouter({
     {
       path: '/entradas',
       name: 'entradas',
-<<<<<<< HEAD
       component: EntradasView
     },
-=======
-      component: EntradasView   
-    }
->>>>>>> af803c59ca55737df649d37c9c75ad11b0ba1fb9
-    
-  ]
-})
+    ]
 
 export default router
