745b38d930f1b477a4b273dc9865460c09a0dbca
g_vallentini <131276482+Vallentini@users.noreply.github.com>
2024-09-25 22:05:27 -0300

    Update EntradasView.vue

--- src/views/EntradasView.vue
+++ src/views/EntradasView.vue
@@ -1,5 +1,4 @@
 <script setup>
-<<<<<<< HEAD
 import { ref } from 'vue'
 import Sunomono from '../assets/Sunomono.png';
 import Gyoza from '../assets/gyoza.png';
@@ -20,7 +19,7 @@ const entries = ref([
     <img src="../assets/bannerEntradas.png" alt="Banner Entradas" class="banner">
     <h2>Entradas</h2>
 
-    <div class="bebidas">
+    <div class="entries">
       <div v-for="entries in entries" :key="entries.name" class="entries-item">
         <img :src="entries.image" :alt="entries.name" class="entries-image">
         <div class="entries-details">
@@ -32,37 +31,6 @@ const entries = ref([
 
           <p class="description">{{ entries.description }}</p>
 
-=======
-import { ref } from 'vue';
-import MenuVue from '@/components/others/MenuVue.vue';
-
-const pratos = [{
-    imagem: '/public/imagem sumono.jpg',
-    titulo: 'Sunomono',
-    descricao: 'Salada refrescante de pepino agridoce com gergelim torrado.'
-},
-{
-    imagem: '/public/imagem sumono.jpg',
-    titulo: 'temaki',
-    descricao: 'ishsofhs'
-}]
-</script>
-
-<template>
-    <MenuVue />
-    <div class="container">
-        <h2>Entradas</h2>
-        <div class="cardapio">
-            <div v-for="prato in pratos" :key="prato.titulo">
-                <div class="pratos">
-                    <img :src="prato.imagem" :alt="prato.titulo" class="Menu">
-                    <div class="descricao">
-                    <h3>{{ prato.titulo }}</h3>
-                    <p>{{ prato.descricao }}</p>
-                </div>
-                </div>
-            </div>
->>>>>>> af803c59ca55737df649d37c9c75ad11b0ba1fb9
         </div>
       </div>
     </div>
@@ -70,7 +38,6 @@ const pratos = [{
 </template>
 
 <style scoped>
-<<<<<<< HEAD
 main {
   width: 60%;
   margin: auto;
@@ -178,63 +145,5 @@ h2 {
   text-align: center;
   letter-spacing: 0.25em;
   color: #FFFFFF;
-=======
-.cardapio {
-    justify-content: center;
-}
-
-.descricao{
-    padding-top: 2%;
-}
-
-.pratos {
-    display: flex;
-    border-radius: 8px;
-    margin-left: 25%;
-    width: 70%;
-    background-color: #454545;
-    margin-top: 2%;
-    color: #FFFFFF;
-}
-
-.Menu {
-    margin: 2%;
-    width: 18%;
-    border-radius: 8px;
-
-}
-
-main {
-    width: 90%;
-    margin: auto;
-    display: flex;
-}
-
-.sidebar {
-    height: 100vh;
-    width: 200px;
-    position: fixed;
-    top: 9, 5vh;
-    left: 0;
-    background-color: #333;
-    padding-top: 20px;
-}
-
-.sidebar ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-.sidebar ul li {
-    padding: 15px;
-    text-align: center;
-}
-
-.sidebar ul li a {
-    text-decoration: none;
-    color: white;
-    display: block;
-    font-size: 18px;
->>>>>>> af803c59ca55737df649d37c9c75ad11b0ba1fb9
 }
 </style>
