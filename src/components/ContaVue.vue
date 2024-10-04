284ff24ccca6885d07a9e2d6e1625ff5bd045e86
Henrique0330 <henriquebelboni@gmail.com>
2024-10-04 09:33:30 -0300

    feat: centralizando a div do ContaVue.vue #8

---
+++ src/components/ContaVue.vue
@@ -0,0 +1,38 @@
+<script setup>
+// import { ref } from 'vue'
+// const contas = ref([
+//     {
+//         id: '1', 
+//         unidade: 'Unidade 1',
+//         produto: 'Produto A',
+//         valor: '100',
+//     },
+//     {
+//         id: '2', 
+//         unidade: 'Unidade 2',
+//         produto: 'Produto B',
+//         valor: '200',
+//     }
+// ]);
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="conta">
+            <div v-for="conta in contas" :key="conta.id">
+                <p>{{ conta.unidade }}, {{ conta.produto }}, {{ conta.valor }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    position: absolute;
+    width: 400px;
+    height: 500%;
+    top: 186px;
+    background-color: blue;
+}
+</style>
--- src/components/others/HeaderVue.vue
+++ src/components/others/HeaderVue.vue
@@ -1,29 +1,53 @@
 <script setup>
-</script>
+import { ref } from 'vue';
+import ContaView from '@/components/ContaVue.vue';
+const showConta = ref(false);
 
+</script>
 <template>
-    <header>
-        <div class="container">
-            <img class="logo-principal" src="/public/logoH.png">
-            <img class="logo" src="/public/logo-sushi.svg">
+  <header>
+    <div class="container">
+      <img class="logo-principal" src="/public/logoH.png">
+      <img class="logo" src="/public/logo-sushi.svg">
+      <div class="cart-icon">
+        <div>
+          <button @click="showConta = !showConta"><img src="/public/logo-carrinho.svg" alt="Carrinho de Compras" />
+          </button>
         </div>
-    </header>
+      </div>
+      <div v-if="showConta">
+        <ContaView />
+      </div>
+    </div>
+  </header>
 </template>
 
 <style scoped>
 header {
-    width: 100%;
-    padding: 20px;
-    margin: auto;
-    background-color: #1a1818e8;
+  width: 100%;
+  padding: 20px;
+  margin: auto;
+  background-color: #1a1818e8;
 }
 
-.container{
-    display: flex;
-    flex-direction: column;
-    width: 50%;
-    margin: auto;
-    justify-content: center;
-    align-items: center;
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: auto;
+  justify-content: center;
+  align-items: center;
+}
+
+.cart-icon {
+  position: absolute;
+  top: 30px;
+  right: 25px;
+}
+
+.cart-icon img {
+  width: 40px;
+  height: auto;
+  cursor: pointer;
 }
 </style>
